8ddd12d52dc0630e0e267c388a68bfe94a014daa
傅立业（Chris Fu） <17433201@qq.com>
2023-07-28 12:34:33 +0800

    Make a Pypi project

--- runtime_keypath/_core.py
+++ runtime_keypath/_core.py
@@ -1,44 +1,46 @@
+# pyright: reportImplicitOverride=false
+
+
 from __future__ import annotations
-
-from typing import TYPE_CHECKING
-
-# region[Exports]
 
 __all__ = [
     "KeyPath",
     "KeyPathSupporting",
 ]
 
-if TYPE_CHECKING:
-    __all__ += []
+# region[Keywords]
 
-# endregion[Exports]
+from typing import TYPE_CHECKING, Final, Generic, Protocol, TypeVar, cast, final
 
-# region[Imports]
-
-from dataclasses import dataclass, field
-from threading import local as ThreadLocal
-from typing import Generic, NamedTuple, TypeVar, cast, final
-
-if TYPE_CHECKING:
-    from collections.abc import Sequence
-    from typing import Any, Protocol
-
-# endregion[Imports]
+# endregion[Keywords]
 
 # region[Types]
 
-_Value_t = TypeVar("_Value_t")
+if TYPE_CHECKING:
+    from typing import Any, Sequence
 
 # endregion[Types]
+
+import threading
+from dataclasses import dataclass, field
+from typing import NamedTuple
+
+_Value_t = TypeVar("_Value_t")
+_Value_co = TypeVar("_Value_co", covariant=True)
+
+
+class _ThreadLocalProtocol(Protocol):
+    recorder: _KeyPathRecorder
+    """
+    The active key-path recorder for this thread. May not exist.
+    """
+
+
+_thread_local = cast("_ThreadLocalProtocol", threading.local())
 
 
 @final
 class _Terminals(NamedTuple):
-    """
-    Records the start point and end point of the key-path chain.
-    """
-
     start: Any
     end: Any
 
@@ -51,39 +53,30 @@ class _KeyPathRecorder:
     busy: bool = False
 
 
-if TYPE_CHECKING:
-
-    class _ThreadLocalProtocol(Protocol):
-        recorder: _KeyPathRecorder
-
-
-_thread_local = cast("_ThreadLocalProtocol", ThreadLocal())
-
-
-# A metaclass is made for class `KeyPath`, and `KeyPath.of` is provided
-# as a property on class `KeyPath`, so that whenever `KeyPath.of` gets
-# accessed, we can do something before it actually gets called.
+# ! A metaclass is made for class `KeyPath`, and `KeyPath.of` is
+# ! provided as a property on class `KeyPath`, so that whenever
+# ! `KeyPath.of` gets accessed, we can do something before it actually
+# ! gets called.
 @final
 class _KeyPathMeta(type):
     @property
-    def of(self) -> _KeyPathOfFunction:
+    def of(self, /) -> _KeyPathOfFunction:
         """
         Returns the key-path for accessing a certain value from a target
-        object with a member chain such as `a.b.c`.
+        object with a key sequence such as `a.b.c`.
 
         The target object and all intermediate objects, except for the
         final value, are expected to subclass `KeyPathSupporting`.
 
         Parameters
         ----------
-        `value` : `_Value_t`
+        `value`
             A value that is accessed with chained keys such as `a.b.c`.
 
         Returns
         -------
-        `KeyPath[_Value_t]`
-            A key-path that indicates the target object and the key
-            sequence to access the given value.
+        A key-path that indicates the target object and the key sequence
+        to access the given value.
 
         Raises
         ------
@@ -99,14 +92,14 @@ class _KeyPathMeta(type):
         ...         self.b = B()
         ...     def __repr__(self) -> str:
         ...         return "a"
-        ...
+
         >>> class B(KeyPathSupporting):
         ...     def __init__(self) -> None:
         ...         self.c = C()
-        ...
+
         >>> class C:
         ...     pass
-        ...
+
         >>> a = A()
         >>> KeyPath.of(a.b.c)
         KeyPath(target=a, keys=('b', 'c'))
@@ -120,8 +113,8 @@ class _KeyPathMeta(type):
             raise RuntimeError(
                 " ".join(
                     [
-                        "An unfinished key-path recorder has been found. Check if",
-                        "`KeyPath.of` is always called immediatelly.",
+                        "An unfinished key-path recorder has been found.",
+                        "Check if `KeyPath.of` is always called immediatelly.",
                     ]
                 )
             )
@@ -133,27 +126,26 @@ class _KeyPathMeta(type):
         return func
 
 
-# We built the result of `KeyPath.of` as a stand-alone class, so that
-# when an exception occurred during the key-path access, there would
-# still be a chance to perform some finalization.
+# ! We implement the result of `KeyPath.of` as a stand-alone class, so
+# ! that when an exception occurred during the key-path access, there
+# ! would still be a chance to perform some finalization.
 class _KeyPathOfFunction:
     """
     Returns the key-path for accessing a certain value from a target
-    object with a member chain such as `a.b.c`.
+    object with a key sequence such as `a.b.c`.
 
     The target object and all intermediate objects, except for the
     final value, are expected to subclass `KeyPathSupporting`.
 
     Parameters
     ----------
-    `value` : `_Value_t`
+    `value`
         A value that is accessed with chained keys such as `a.b.c`.
 
     Returns
     -------
-    `KeyPath[_Value_t]`
-        A key-path that indicates the target object and the key sequence
-        to access the given value.
+    A key-path that indicates the target object and the key sequence to
+    access the given value.
 
     Raises
     ------
@@ -169,23 +161,23 @@ class _KeyPathOfFunction:
     ...         self.b = B()
     ...     def __repr__(self) -> str:
     ...         return "a"
-    ...
+
     >>> class B(KeyPathSupporting):
     ...     def __init__(self) -> None:
     ...         self.c = C()
-    ...
+
     >>> class C:
     ...     pass
-    ...
+
     >>> a = A()
     >>> KeyPath.of(a.b.c)
     KeyPath(target=a, keys=('b', 'c'))
     """
 
-    __called: bool = False
+    __invoked: bool = False
 
     def __call__(self, value: _Value_t, /) -> KeyPath[_Value_t]:
-        self.__called = True
+        self.__invoked = True
 
         try:
             recorder = _thread_local.recorder
@@ -193,8 +185,8 @@ class _KeyPathOfFunction:
             raise RuntimeError(
                 " ".join(
                     [
-                        "`KeyPath.of` must be used directly and should NOT be saved",
-                        "and then called more than once.",
+                        "`KeyPath.of` must be accessed and then called immediatedly",
+                        "and should NOT be called more than once.",
                     ]
                 )
             )
@@ -216,8 +208,8 @@ class _KeyPathOfFunction:
                 raise RuntimeError(
                     " ".join(
                         [
-                            "Key-path is broken. Check if there is something that",
-                            "does NOT support key-paths in the member chain.",
+                            "Key-path is broken. Check if there is something that does",
+                            "NOT support key-paths in the member chain.",
                         ]
                     )
                 )
@@ -225,17 +217,20 @@ class _KeyPathOfFunction:
         key_path = KeyPath(terminals.start, key_list)
         return key_path
 
-    def __del__(self) -> None:
-        # If an exception had occured during the key-path access, or
-        # this function were just discarded without being  called, we
-        # would do some clean-up here.
-        if not self.__called:
+    def __del__(self, /) -> None:
+        # ! If an exception had occured during the key-path access, or
+        # ! this function were just discarded without being finally
+        # ! called, we would do some cleaning here.
+        if not self.__invoked:
             del _thread_local.recorder
 
 
 @final
-class KeyPath(Generic[_Value_t], metaclass=_KeyPathMeta):
-    def __init__(self, target: Any, keys: str | Sequence[str]) -> None:
+class KeyPath(Generic[_Value_co], metaclass=_KeyPathMeta):
+    __target: Final[object]
+    __keys: Final[Sequence[str]]
+
+    def __init__(self, /, target: object, keys: str | Sequence[str]) -> None:
         self.__target = target
 
         if isinstance(keys, str):
@@ -245,14 +240,14 @@ class KeyPath(Generic[_Value_t], metaclass=_KeyPathMeta):
         self.__keys = keys
 
     @property
-    def target(self) -> Any:
+    def target(self, /) -> object:
         return self.__target
 
     @property
-    def keys(self) -> tuple[str, ...]:
+    def keys(self, /) -> Sequence[str]:
         return self.__keys
 
-    def __hash__(self) -> int:
+    def __hash__(self, /) -> int:
         return hash((self.target, self.keys))
 
     def __eq__(self, other: object, /) -> bool:
@@ -262,59 +257,62 @@ class KeyPath(Generic[_Value_t], metaclass=_KeyPathMeta):
             and self.keys == other.keys
         )
 
-    def __repr__(self) -> str:
+    def __repr__(self, /) -> str:
         return f"{KeyPath.__name__}(target={self.target!r}, keys={self.keys!r})"
 
-    def __call__(self) -> _Value_t:
+    def __call__(self, /) -> _Value_co:
         value = self.__target
         for key in self.__keys:
             value = getattr(value, key)
-        return value
+        return cast("_Value_co", value)
 
 
 class KeyPathSupporting:
-    # `__getattribute__(...)` is declared against `TYPE_CHECKING`, so
-    # that unknown attributes on conforming classes won't be regarded as
-    # known by type-checkers.
-    if not TYPE_CHECKING:
+    # ! This method is purposedly not named as `__getattribute__`. See below for reason.
+    def __getattribute_0__(self, key: str, /) -> Any:
+        try:
+            recorder = _thread_local.recorder
+        except AttributeError:
+            # There is no recorder, which means that `KeyPath.of` is not
+            # being called. So we don't need to record this key.
+            return super().__getattribute__(key)
 
-        def __getattribute__(self, key: str, /) -> Any:
-            try:
-                recorder = _thread_local.recorder
-            except AttributeError:
-                # There is no recorder, which means that `KeyPath.of` is
-                # not being called. So we don't need to record this key.
-                return super().__getattribute__(key)
+        if recorder.busy:
+            # The recorder is busy, which means that another member is
+            # being accessed, typically because the computation of that
+            # member is dependent on this one. So we don't need to
+            # record this key.
+            return super().__getattribute__(key)
 
-            if recorder.busy:
-                # The recorder is busy, which means that another member
-                # is being accessed, typically because the computation
-                # of that member is dependent on this one. So we don't
-                # need to record this key.
-                return super().__getattribute__(key)
+        recorder.busy = True
 
-            recorder.busy = True
-
-            terminals = recorder.terminals
-            if terminals is not None and terminals.end is not self:
-                raise RuntimeError(
-                    " ".join(
-                        [
-                            "Key-path is broken. Check if there is something that does",
-                            "NOT support key-paths in the member chain.",
-                        ]
-                    )
+        terminals = recorder.terminals
+        if terminals is not None and terminals.end is not self:
+            raise RuntimeError(
+                " ".join(
+                    [
+                        "Key-path is broken. Check if there is something that does NOT",
+                        "support key-paths in the member chain.",
+                    ]
                 )
+            )
 
-            value = super().__getattribute__(key)
+        value = super().__getattribute__(key)
 
-            if terminals is None:
-                terminals = _Terminals(self, value)
-            else:
-                terminals = terminals._replace(end=value)
+        if terminals is None:
+            terminals = _Terminals(self, value)
+        else:
+            terminals = terminals._replace(end=value)
 
-            recorder.terminals = terminals
-            recorder.key_list.append(key)
-            recorder.busy = False
+        recorder.terminals = terminals
+        recorder.key_list.append(key)
+        recorder.busy = False
 
-            return value
+        return value
+
+    # ! `__getattribute__(...)` is declared against `TYPE_CHECKING`, so that unknown
+    # ! attributes on conforming classes won't be regarded as known by type-checkers.
+    if not TYPE_CHECKING:
+        __getattribute__ = __getattribute_0__
+
+    del __getattribute_0__
